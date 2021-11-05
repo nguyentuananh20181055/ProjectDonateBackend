9b75d9d0a0a97683f88b320652845eacafe77dc8
nguyentuananh20181055 <92306754+nguyentuananh20181055@users.noreply.github.com>
2021-11-05 18:37:01 +0700

    ProjectDonateBackend

---
+++ src/main/java/entities/DonateInfo.java
@@ -0,0 +1,12 @@
+package entities;
+
+public class DonateInfo {
+    public String name;
+    public int money;
+    public String comment;
+    public long timeStamp;
+
+    public DonateInfo() {
+
+    }
+}
